f3bd739c06105e1b66cb3c3c4919783d1992c87c
TL <TL@gmail.com>
2020-04-15 08:48:54 +0530

    mvc

---
+++ MVCAnnno12-ShomeHomePage/src/main/java/com/nt/controller/ShowHomeController.java
@@ -0,0 +1,13 @@
+package com.nt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ShowHomeController {
+
+	 @RequestMapping("/home.htm")
+	public String  home() {
+		return "welcome";
+	}
+}
---
+++ MVCAnnno12-ShomeHomePage/src/main/webapp/WEB-INF/pages/welcome.jsp
@@ -0,0 +1,5 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"  isELIgnored="false"%>
+
+    
+    <h1 style="color:blue;text-align:center">Welcome to Spring MVC</h1>
