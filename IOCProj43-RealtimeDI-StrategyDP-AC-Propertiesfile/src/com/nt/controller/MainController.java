0d5fd0fa059efa58b4b1f7d0cb9671816c990317
TL <TL@gmail.com>
2020-04-24 19:51:02 +0530

    IOC

---
+++ IOCProj43-RealtimeDI-StrategyDP-AC-Propertiesfile/src/com/nt/controller/MainController.java
@@ -0,0 +1,34 @@
+package com.nt.controller;
+
+import com.nt.dto.StudentDTO;
+import com.nt.service.StudentOperationsService;
+import com.nt.vo.StudentVO;
+
+public final class MainController {
+	private StudentOperationsService service;
+
+	public MainController(StudentOperationsService service) {
+		System.out.println("MainController::1-param constructor");
+		this.service = service;
+	}
+	
+	public String  handleStudent(StudentVO vo)throws Exception{
+		StudentDTO dto=null;
+		String result=null;
+		//convert VO to DTO
+		dto=new StudentDTO();
+	
+		dto.setSname(vo.getSname());
+		dto.setSadd(vo.getSadd());
+		dto.setM1(Integer.parseInt(vo.getM1()));
+		dto.setM2(Integer.parseInt(vo.getM2()));
+		dto.setM3(Integer.parseInt(vo.getM3()));
+		//use service
+		result=service.register(dto);
+		return result;
+		
+		
+	}
+	
+
+}
---
+++ IOCProj43-RealtimeDI-StrategyDP-AC-Propertiesfile/src/com/nt/test/RealtimeDITest_AC.java
@@ -0,0 +1,59 @@
+package com.nt.test;
+
+import java.io.Closeable;
+import java.util.Scanner;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.nt.controller.MainController;
+import com.nt.vo.StudentVO;
+
+public class RealtimeDITest_AC {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx=null;
+		MainController controller=null;
+		Scanner sc=null;
+		String sname=null,sadd=null,m1=null,m2=null,m3=null;
+		StudentVO vo=null;
+		String result=null;
+				//create IOC container
+		ctx=new ClassPathXmlApplicationContext("com/nt/cfgs/applicationContext.xml");
+		//get Controller class object
+		controller=ctx.getBean("controller",MainController.class);
+		//read inputs 
+		sc=new Scanner(System.in);
+		System.out.println("Enter student name::");
+		sname=sc.next();
+		System.out.println("Enter student address::");
+		sadd=sc.next();
+		System.out.println("Enter student Marks1::");
+		m1=sc.next();
+		System.out.println("Enter student Marks2::");
+		m2=sc.next();
+		System.out.println("Enter student Marks3::");
+		m3=sc.next();
+		//prepare VO class object
+		vo=new StudentVO();
+		 vo.setSname(sname); vo.setSadd(sadd);
+		vo.setM1(m1); vo.setM2(m2); vo.setM3(m3);
+		
+		//invoke method
+		try {
+			result=controller.handleStudent(vo);
+			System.out.println(result);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("STudent Registration failed");
+		}
+		
+		
+		((AbstractApplicationContext) ctx).close();
+		
+		
+
+	}//main
+}//class
